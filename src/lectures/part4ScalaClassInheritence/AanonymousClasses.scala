f0edbec18f0f652a2315a82f8289bb45340d0869
SwagarikaGiri <swagaarika95@gmail.com>
2020-06-06 21:41:57 +0530

    Anonymous Classes - Variant Problem- Generics types Example aand Exercise Practised

---
+++ src/lectures/part4ScalaClassInheritence/AanonymousClasses.scala
@@ -0,0 +1,5 @@
+package lectures.part4ScalaClassInheritence
+
+object AanonymousClasses extends App{
+
+}
---
+++ src/lectures/part4ScalaClassInheritence/InheritenceExercise.scala
@@ -0,0 +1,5 @@
+package lectures.part4ScalaClassInheritence
+
+object InheritenceExercise extends App{
+
+}
