78022f8dc4e6abbec54b16c9b65cb28fccc275c0
SwagarikaGiri <swagaarika95@gmail.com>
2020-06-09 10:19:59 +0530

    Introduction to scala with cat framework

--- src/playground/ScalaPlayGround.scala
+++
@@ -1,6 +0,0 @@
-package playground
-
-object ScalaPlayGround extends App{
-  println("Hello Scala")
-
-}
---
+++ src/scalaWithCatFramework/Part1IntroToCatFrameWork.scala
@@ -0,0 +1,42 @@
+package scalaWithCatFramework
+
+object Part1IntroToCatFrameWork extends App {
+  trait Printable[T]{
+    def format(value:T):String
+  }
+
+  /*
+  Type Class instances provide the
+  implementation for the type
+  implicit means that can be used directly
+   */
+   object PrintableInstances{
+    implicit val intInstance = new Printable[Int] {
+      override def format(value: Int): String = value.toString
+    }
+
+    implicit val stringInstance =  new Printable[String] {
+      override def format(value: String): String = s"Hello, It is a string instance $value"
+    }
+  }
+/*
+   Type cast interface exposes the functionality
+   for the user
+   It takes the instance of type cast as a parameter
+   So printable knows only about printable trait/ type cast
+
+
+ */
+  object PrintableInterface{
+  def print[T](value:T)(implicit printable:Printable[T]):Unit= println(printable.format(value))
+}
+  import PrintableInstances._
+
+PrintableInterface.print("Swagarika Giri")
+  /*
+  There is another thing pip my library beautiful methods
+   */
+
+
+
+}
